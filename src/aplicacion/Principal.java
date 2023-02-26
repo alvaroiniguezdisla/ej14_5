ab6a9f96acbd747d87c32751022f156406763c7b
Álvaro <ainiguezdislaa@gmail.com>
2023-02-26 17:47:28 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,9 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej14_5;
+public class Principal {
+    public static void main(String[] args) {
+        int[]lista= {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("La media es: " + dominio.ej14_5.media(lista));
+
+    }
 }
--- src/dominio/ej14_5.java
+++ src/dominio/ej14_5.java
@@ -1,2 +1,9 @@
-package dominio;public class ej14_5 {
+package dominio;
+import  java.util.stream.Stream;
+
+public class ej14_5 {
+    public static int media(int[] lista) {
+        return Stream.iterate(0, i -> i + 1).limit(lista.length).map(i -> lista[i]).reduce(0, (a, b) -> a + b) / lista.length;
+    }
+
 }
